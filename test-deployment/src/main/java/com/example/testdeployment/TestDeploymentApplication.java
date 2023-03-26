b0533c60cfbaaca7c332d8c1cbac93972f3d4d15
Kevin Halili <kevin.halili@ids.al>
2023-03-26 22:27:32 +0200

    retry

---
+++ test-deployment/src/main/java/com/example/testdeployment/TestDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploymentApplication.class, args);
+	}
+
+}
