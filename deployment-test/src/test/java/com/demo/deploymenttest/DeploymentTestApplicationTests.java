a1c2cc19b1004b6b404fa78e3cbe6f14cbabcef6
Kevin Halili <kevin.halili@ids.al>
2023-03-26 21:03:21 +0200

    initial

---
+++ deployment-test/src/test/java/com/demo/deploymenttest/DeploymentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.deploymenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
